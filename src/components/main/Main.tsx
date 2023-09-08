f7e3a71e4a77c609bc1110f7141f0739f73b415d
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-09 00:44:48 +0200

    Add tailwind

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -8,10 +8,15 @@ export type MainProps = {
 export default component$<MainProps>(
   ({ title = 'Won Games', description = 'Purchase online games' }) => {
     return (
-      <main>
-        <img src="img/hero-illustration.svg" />
-        <h1>{title}</h1>
-        <h2>{description}</h2>
+      <main class='bg-blue-950 text-white w-full h-full p-12 text-center flex flex-col items-center justify-center'>
+        <h1 class='text-4xl'>{title}</h1>
+        <h2 class='text-3xl font-normal'>{description}</h2>
+        {/* eslint-disable-next-line qwik/jsx-img*/}
+        <img
+          class='mt-12'
+          src='img/hero-illustration.svg'
+          style={{ width: 'min(30rem, 100%)', height: 'auto' }}
+        />
       </main>
     );
   }
