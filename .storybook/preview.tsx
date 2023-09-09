f2585375ebfb0f8fbe5fbe1bc72a3e6a4d7f8c48
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-09 12:08:06 +0200

    Remove comments

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -19,8 +19,6 @@ export const parameters: Parameters = {
 };
 
 export const decorators = [
-  // Adds theme switching support.
-  // NOTE: requires setting "darkMode" to "class" in your tailwind config
   withThemeByClassName({
     themes: {
       light: 'light',
